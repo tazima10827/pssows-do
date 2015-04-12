09580872387c818fe3af8cc29599a4c60e00dd24
田嶋智洋 <kapiha7@gmail.com>
2015-04-12 21:16:39 +0900

    今日はパスワード解析アプリ

--- pssows-do/ViewController.h
+++ pssows-do/ViewController.h
@@ -10,6 +10,19 @@
 
 @interface ViewController : UIViewController
 
+{
+        IBOutlet UILabel *resultLabel1 ;   //結果(1番右のラベル)
+        IBOutlet UILabel *resultLabel2 ;   //結果(2番右のラベル)
+        IBOutlet UILabel *resultLabel3 ;   //結果(3番右のラベル)
+        IBOutlet UILabel *resultLabel4 ;   //結果(4番右のラベル)
+        IBOutlet UILabel *resultLabel5 ;   //結果(5番右のラベル)
+        IBOutlet UILabel *countLabel ;   //結果(1番右のラベル)
+        IBOutlet UIBarButtonItem *start ;   //結果(1番右のラベル)
+    
+    int pas;
+    int password;
+}
+
 
 @end
 
--- pssows-do/ViewController.m
+++ pssows-do/ViewController.m
@@ -17,7 +17,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+        resultLabel1.text = @"0";
+        resultLabel2.text = @"0";
+        resultLabel3.text = @"0";
+        resultLabel4.text = @"0";
+        resultLabel5.text = @"0";
+        //countLabel.text = @"0";
+        password = arc4random() % 90000;
+
 }
+
+
+ -(IBAction)start {
+ //1が10000より小さい間ループ。
+     for(int i=0 ;i<90000;i = i+1){
+         //カウント中の数字をcountLabelにする
+         int k = i;
+          int digit[5];
+         for (int j = 0 ; j < 5 ; j++){
+             digit[j] = (k % 10); //j桁目の数字
+             k =k / 10;
+         }
+
+         resultLabel1.text = [NSString stringWithFormat:@"%d",digit[0]];
+         resultLabel2.text = [NSString stringWithFormat:@"%d",digit[1]];
+         resultLabel3.text = [NSString stringWithFormat:@"%d",digit[2]];
+         resultLabel4.text = [NSString stringWithFormat:@"%d",digit[3]];
+         resultLabel5.text = [NSString stringWithFormat:@"%d",digit[4]];
+         
+        
+         //処理を0.0005秒、待機する。
+         [[NSRunLoop currentRunLoop]
+          runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.0000009]
+          ];
+         
+         if(i == password){ //passwordと一致した場合
+             break;
+         }
+     }
+ 
+ }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
